547d830cae2662e34159441daffa5ed43d48f559
pie-and-cat <shubeksha.acharya@gmail.com>
2017-05-16 17:02:57 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,57 +1,74 @@
-<!--https://www.w3schools.com/js/js_intro.asp -->
-
-// the one line functions (aka the way I rename the function so i can better understand what is going on)
-//function refresh(){
-//	window.location.reload();
-//}
-	//the function for 
-	/*function validation()
-	{
-		var colorsName = ["Red","Blue","Purple","Green"];
-		for (var notx=0;notx < 1;notx++){
-			var printColorName = colorsName[Math.round(Math.random()*colorsName.length-1);}
-			onclick= document.getElementByClassName('square').style.color = "Green";
-		if (printColorName=="Purple"){
-			refresh();
-		}
-	} */
-function random(){
-	var Color;
+window.onload = randomText()
+console.log("onLoad");
+;
+  function randomText(){
+	//var Color;
 	var colorsName =["red","blue","purple","green"];
 	var printColorName = colorsName[Math.floor(Math.random()*colorsName.length)];
 	console.log("Yo random is working");
-	//validation(printColorName);
-	changeText(printColorName);
-}
-	
+	document.getElementById("question").innerHTML=colorsName[Math.floor(Math.random()*colorsName.length)];
+	changeTextColor();
+  }
 
-function changeText(printColorName){
+function changeTextColor(){
 	//change text
-	document.getElementById("question").innerHTML = printColorName;
 	// change colors
 	var Color;
-	switch (printColorName){
-	case printColorName == "red":
-			Color = red;
-	case printColorName == "blue":
-			Color = blue;
-	case printColorName == "purple":
-			Color = purple;
-	case printColorName == "green":
-			Color = green;
-	default:
+	var textEquals = document.getElementById("question").textContent;
+	//document.getElementById("spanx").innerHTML=textEquals;
+	switch (textEquals){
+	case "red":
+			Color = "red";
 			break;
+	case "blue":
+			Color = "blue";
+			break;
+	case "purple":
+			Color = "purple";
+			break;
+	case "green":
+			Color = "green";
+			break;
+	//default: Color = "cyan";
 	}
-	document.getElementById("question").style.color = printColorName;
-	console.log("Yo changeText is working");
+	document.getElementById("question").style.color = Color;
+	console.log("Yo changeTextColor is working");
 }
-function check(clicked_id){
-	//var clickedOn = clicked_id;
-	var GEBIid = document.getElementById(clicked_id);
-	var GEBIq = document.getElementById("question");
-	console.log("THE IF STATEMENT TO GET IT TO WORK IS WORKING");
-	if (GEBIid.style.backgroundColor==GEBIq.style.color){
-		//window.location.reload();
-		random();
+function defBackGrownd(message){
+	document.getElementById("oval").backgroundColor = "purple";
+	document.getElementById("square").backgroundColor = "red";
+	document.getElementById("circle").backgroundColor = "green";
+	document.getElementById("parallelogram").backgroundColor = "blue";
+	switch (message){
+	case "oval":
+			document.getElementById("oval").backgroundColor = "purple";
+			break;
+	case "square":
+			document.getElementById("square").backgroundColor = "red";
+			break;
+	case "circle":
+			document.getElementById("circle").backgroundColor = "green";
+			break;
+	case "parallelogram":
+			document.getElementById("parallelogram").backgroundColor = "blue";
+			break;
+	//default: Color = "cyan";
+	}
+	//var colorOfCId=clicked_id.style.backgroundColor;
+	//console.log(clicked_id.style.backgroundColor.value);
+	console.log("defBackGrownd() works");
+}
+
+function checkQuestion(clicked_id){
+	var clickedIdString = String(clicked_id);
+	var clickedId = document.getElementById(clickedIdString);
+	var style1 = window.getComputedStyle(clickedId,null).getPropertyValue('background-color');	//console.log(stylezz);
+	var elem2 = document.getElementById("question");
+	var style2 = window.getComputedStyle(elem2,null).getPropertyValue('color');
+	if(style1==style2){
+		console.log("the if statement is working");
+		randomText();
+	}else{
+		console.log("the if statement is working");
 	}
 }
